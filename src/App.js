67200a90018a7b787af360fad7daa9ccc913263e
melisaailensalazar <67084450+melisaailensalazar@users.noreply.github.com>
2022-08-25 15:30:32 -0300

    Botón contador

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from "react";
 import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/Productos/ItemListContainer";
 import "bootstrap/dist/css/bootstrap.min.css";
+import ItemCount from "./components/Productos/ItemCount";
 
 function App() {
+  const onAdd = (qty) => {
+    alert(`Agregaste ${qty} productos`);
+  };
   return (
     <>
       <NavBar />
+      <ItemCount onAdd={onAdd} initial={1} stock={7} />
     </>
   );
 }
--- src/components/NavBar/CartWidget.js
+++ src/components/NavBar/CartWidget.js
@@ -1,13 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import qty from "../Productos/ItemCount";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import "../styles/CartWidget.css";
 
 const CartWidget = () => {
   return (
     <div className="cart">
       <FontAwesomeIcon name="cart" icon={faCartShopping} />
-      <span class="item__total">0</span>
+      <span class="qty-display">0</span>
     </div>
   );
 };
